805e8f9752c4e2a2a2cd91b02d42ce010a035152
alfu-git <mdalfaz.dev@gmail.com>
2026-05-03 18:15:43 +0600

    replace products data url

--- src/lib/dataFetch.js
+++ src/lib/dataFetch.js
@@ -1,4 +1,4 @@
 export const getAllProducts = async () => {
-  const res = await fetch("https://data-hazel-one.vercel.app/data.json");
+  const res = await fetch("https://m-48-a-08-suncart.vercel.app/data.json");
   return res.json();
 };
